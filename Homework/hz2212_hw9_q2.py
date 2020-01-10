cabf20a3dc7c05a20d00c083896bbeea1000dbd4
Hiroki39 <zhenghongyi2000@hotmail.com>
2020-01-10 18:01:33 +0800

    modify hz2212_hw9_q2.py

--- Homework/hz2212_hw9_q2.py
+++ Homework/hz2212_hw9_q2.py
@@ -3,7 +3,11 @@ sys.path.append(sys.path[0] + '/../DataStructure')
 from ChainingHashTableMap import ChainingHashTableMap
 
 
-def intersection_list(lst1, lst2):
+def intersection_list1(lst1, lst2):
+    return [elem for elem in lst1 if elem in lst2]
+
+
+def intersection_list2(lst1, lst2):
     element_map = ChainingHashTableMap()
     for elem in lst1:
         element_map[elem] = None
@@ -11,7 +15,7 @@ def intersection_list(lst1, lst2):
     for elem in lst2:
         try:
             element_map[elem]
-            int_lst.append()
+            int_lst.append(elem)
         except KeyError:
             pass
     return int_lst
