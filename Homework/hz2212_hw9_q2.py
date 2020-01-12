872b507b8c60772f1d9b558e2a6b477cfd0bde7e
Hiroki39 <zhenghongyi2000@hotmail.com>
2020-01-12 23:50:43 +0800

    modify hw9q2

--- Homework/hz2212_hw9_q2.py
+++ Homework/hz2212_hw9_q2.py
@@ -1,10 +1,26 @@
 import sys
 sys.path.append(sys.path[0] + '/../DataStructure')
+sys.path.append(sys.path[0] + '/../Algorithm')
 from ChainingHashTableMap import ChainingHashTableMap
+from MergeSort import merge_sort
 
 
 def intersection_list1(lst1, lst2):
-    return [elem for elem in lst1 if elem in lst2]
+    merge_sort(lst1)
+    merge_sort(lst2)
+    ind1 = 0
+    ind2 = 0
+    int_lst = []
+    while ind1 < len(lst1) and ind2 < len(lst2):
+        if lst1[ind1] < lst2[ind2]:
+            ind1 += 1
+        elif lst1[ind1] > lst2[ind2]:
+            ind2 += 1
+        else:
+            int_lst.append(lst1[ind1])
+            ind1 += 1
+            ind2 += 1
+    return int_lst
 
 
 def intersection_list2(lst1, lst2):
