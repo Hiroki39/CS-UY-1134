56194d7f6e4257b51e9234b8a20b9d363711fa2a
Hiroki39 <zhenghongyi2000@hotmail.com>
2020-06-28 01:07:09 -0400

    Add FlipTreeMap

--- DataStructure/AVLFlipTreeMap.py
+++ DataStructure/AVLFlipTreeMap.py
@@ -1,8 +1,9 @@
-class AVLTreeMap:
+class AVLFlipTreeMap:
     class Item:
-        def __init__(self, key, value=None):
+        def __init__(self, key, value=None, flip=False):
             self.key = key
             self.value = value
+            self.flip = flip
 
     class Node:
         def __init__(self, item):
@@ -10,13 +11,6 @@ class AVLTreeMap:
             self.left = None
             self.right = None
             self.height = 0
-            self.parent = None
-
-        def disconnect(self):
-            self.item = None
-            self.left = None
-            self.right = None
-            self.height = None
             self.parent = None
 
         def get_diff(self):
@@ -53,7 +47,9 @@ class AVLTreeMap:
     def __getitem__(self, key):  # return the value associated with key
         node = self.find(key)
         if node is None:
-            raise KeyError(str(key) + " not found")
+            return -1
+        if self.check_flip(node):
+            return -node.item.value
         return node.item.value
 
     def find(self, key):
@@ -70,13 +66,16 @@ class AVLTreeMap:
     def __setitem__(self, key, value):
         node = self.find(key)
         if node is not None:
-            node.item.value = value
+            if self.check_flip(node):
+                node.item.value = -value
+            else:
+                node.item.value = value
         else:
             self.insert(key, value)
 
-    def insert(self, key, value=None):  # assume key not in tree
-        new_item = AVLTreeMap.Item(key, value)
-        new_node = AVLTreeMap.Node(new_item)
+    def insert(self, key, value=None, flip=False):  # assume key not in tree
+        new_item = AVLFlipTreeMap.Item(key, value, flip)
+        new_node = AVLFlipreeMap.Node(new_item)
         parent = None
         curr_node = self.root
         if curr_node is None:
@@ -93,6 +92,8 @@ class AVLTreeMap:
             else:
                 parent.right = new_node
             new_node.parent = parent
+            if self.check_flip(new_node):
+                new_node.item.value *= -1
             self.fix_avl_property(new_node.parent)
         self.size += 1
 
@@ -107,7 +108,11 @@ class AVLTreeMap:
         if node.get_diff() > 0:
             if node.right.get_diff() == -1:
                 self.rotate(node.right)
+
             new_root = node.right
+            self.pushdown(node)
+            self.pushdown(new_root)
+
             node.right = new_root.left
             if new_root.left is not None:
                 new_root.left.parent = node
@@ -115,7 +120,11 @@ class AVLTreeMap:
         else:
             if node.left.get_diff() == 1:
                 self.rotate(node.left)
+
             new_root = node.left
+            self.pushdown(node)
+            self.pushdown(new_root)
+
             node.left = new_root.right
             if new_root.right is not None:
                 new_root.right.parent = node
@@ -135,47 +144,71 @@ class AVLTreeMap:
             self.root = new_root
         return new_root
 
-    def __delitem__(self, key):
-        node = self.find(key)
-        if node is None:
-            raise KeyError(str(key) + " not found")
-        else:
-            self.delete(node)
+    def pushdown(self, node):  # make sure the node involved in the rotation is not flipped
+        if node.item.flip:
+            node.item.flip = False
+            node.item.value *= -1
+            if node.left is not None:
+                node.left.item.flip ^= True
+            if node.right is not None:
+                node.right.item.flip ^= True
 
-    def delete(self, node_to_delete):  # assume key is in the tree
-        item = node_to_delete.item
-        parent = node_to_delete.parent
-        # replace the content of the node with the content of the node with the largest key in the left tree
-        if node_to_delete.left is not None and node_to_delete.right is not None:
-            max_in_left = self.subtree_max(node_to_delete.left)
-            node_to_delete.item = max_in_left.item
-            self.delete(max_in_left)
-        else:
-            if node_to_delete.left is None and node_to_delete.right is None:
-                child = None
-            elif node_to_delete.left is not None and node_to_delete.right is None:
-                child = node_to_delete.left
+    def check_flip(self, node):
+        flipped = False
+        while node is not None:
+            flipped ^= node.item.flip
+            node = node.parent
+        return flipped
+
+    def flip(self, low, high):
+        if high < low:
+            low, high = high, low
+
+        if self.root is None:
+            return
+
+        subtree_root = self.root
+        while not low <= subtree_root.item.key <= high:
+            if subtree_root.item.key < low:
+                subtree_root = subtree_root.right
             else:
-                child = node_to_delete.right
-            if parent is None:
-                self.root = child
-            else:
-                if parent.left is node_to_delete:
-                    parent.left = child
+                subtree_root = subtree_root.left
+            if subtree_root is None:
+                return
+
+        subtree_root.item.value *= -1
+
+        if subtree_root.item.key != low:
+            low_cursor = subtree_root.left
+            while low_cursor is not None and low_cursor.item.key != low:
+                if low_cursor.item.key > low:
+                    if low_cursor.right is not None:
+                        low_cursor.right.item.flip ^= True
+                    low_cursor.item.value *= -1
+                    low_cursor = low_cursor.left
                 else:
-                    parent.right = child
-            if child is not None:
-                child.parent = parent
-            node_to_delete.disconnect()
-            self.fix_avl_property(parent)
-            self.size -= 1
-        return item  # returns item in the node that is removed
+                    low_cursor = low_cursor.right
 
-    def subtree_max(self, subtree_root):
-        curr = subtree_root
-        while curr.right is not None:
-            curr = curr.right
-        return curr
+            if low_cursor is not None:
+                if low_cursor.right is not None:
+                    low_cursor.right.item.flip ^= True
+                low_cursor.item.value *= -1
+
+        if subtree_root.item.key != high:
+            high_cursor = subtree_root.right
+            while high_cursor is not None and high_cursor.item.key != high:
+                if high_cursor.item.key < high:
+                    if high_cursor.left is not None:
+                        high_cursor.left.item.flip ^= True
+                    high_cursor.item.value *= -1
+                    high_cursor = high_cursor.right
+                else:
+                    high_cursor = high_cursor.left
+
+            if high_cursor is not None:
+                if high_cursor.left is not None:
+                    high_cursor.left.item.flip ^= True
+                high_cursor.item.value *= -1
 
     def inorder(self):  # yield references
         def subtree_inorder(root):
